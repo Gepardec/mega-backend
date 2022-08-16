5fe3529d696f5c882d07e78c55482d4a1d81c5f2
Michael Leitner <michael.leitner@gepardec.com>
2022-08-16 13:12:22 +0200

    Delayed mailer reminder

--- src/main/java/com/gepardec/mega/notification/mail/Mail.java
+++ src/main/java/com/gepardec/mega/notification/mail/Mail.java
@@ -4,7 +4,7 @@ public enum Mail {
     EMPLOYEE_CHECK_PROJECTTIME(1, MailType.WORKING_DAY_BASED, "emails/reminder-template.html"),
     OM_CONTROL_EMPLOYEES_CONTENT(3, MailType.WORKING_DAY_BASED, "emails/reminder-template.html"),
     PL_PROJECT_CONTROLLING(5, MailType.WORKING_DAY_BASED, "emails/reminder-template.html"),
-    OM_RELEASE(8, MailType.WORKING_DAY_BASED, "emails/reminder-template.html"),
+    OM_RELEASE(-5, MailType.WORKING_DAY_BASED, "emails/reminder-template.html"),
     OM_ADMINISTRATIVE(15, MailType.DAY_OF_MONTH_BASED, "emails/reminder-template.html"),
     OM_SALARY(-3, MailType.WORKING_DAY_BASED, "emails/reminder-template.html"),
     OM_CONTROL_PROJECTTIMES(-1, MailType.WORKING_DAY_BASED, "emails/reminder-template.html"),
