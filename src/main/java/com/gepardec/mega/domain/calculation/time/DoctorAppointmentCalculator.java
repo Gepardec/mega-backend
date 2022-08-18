685fd92220ef36eb94668644f554c1d7569c90c5
Michael Leitner <michael.leitner@gepardec.com>
2022-08-18 12:02:30 +0200

    Changed language and style

--- src/main/java/com/gepardec/mega/domain/calculation/time/DoctorAppointmentCalculator.java
+++ src/main/java/com/gepardec/mega/domain/calculation/time/DoctorAppointmentCalculator.java
@@ -56,7 +56,6 @@ public class DoctorAppointmentCalculator extends AbstractTimeWarningCalculationS
     private TimeWarning createTimeWarning(ProjectTimeEntry absence){
         TimeWarning timeWarning = new TimeWarning();
         timeWarning.setDate(absence.getDate());
-        timeWarning.getWarnings().add("Warnung: Doktorbuchung außerhalb der Sollarbeitszeit");
         timeWarning.getWarningTypes().add(TimeWarningType.WRONG_DOCTOR_APPOINTMENT);
 
         return timeWarning;
