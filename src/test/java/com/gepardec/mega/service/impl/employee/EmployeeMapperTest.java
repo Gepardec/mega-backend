9422b660ce2bc88a6dfca050ba730627a135b1f1
Simon Bauer <simon.bauer@gepardec.com>
2023-03-22 08:49:57 +0100

    [Gepardec/mega#644] EmploymentPeriods & ExitDate Ermittlungsmethoden vom EmployeeMapper - Unit Tests

--- src/test/java/com/gepardec/mega/service/impl/employee/EmployeeMapperTest.java
+++ src/test/java/com/gepardec/mega/service/impl/employee/EmployeeMapperTest.java
@@ -1,16 +1,24 @@
 package com.gepardec.mega.service.impl.employee;
 
+import com.gepardec.mega.domain.model.DateRange;
 import com.gepardec.mega.domain.model.Employee;
 import com.gepardec.mega.domain.utils.DateUtils;
 import com.gepardec.mega.service.mapper.EmployeeMapper;
 import de.provantis.zep.BeschaeftigungszeitListeType;
 import de.provantis.zep.BeschaeftigungszeitType;
 import de.provantis.zep.MitarbeiterType;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -164,6 +172,229 @@ class EmployeeMapperTest {
         assertThat(actual.getWorkDescription()).isEqualTo(employee.getPreisgruppe());
         assertThat(actual.getReleaseDate()).isEqualTo(employee.getFreigabedatum());
         assertThat(actual.isActive()).isTrue();
+    }
+
+    @Nested
+    @DisplayName("Tests for EmploymentPeriods")
+    class EmploymentPeriods {
+
+        LocalDate _2015_01_01 = LocalDate.of(2015, 1, 1);
+        LocalDate _2010_01_01 = LocalDate.of(2010, 1, 1);
+        LocalDate _2023_01_01 = LocalDate.of(2023, 1, 1);
+        LocalDate _2023_01_25 = LocalDate.of(2023, 1, 25);
+
+        @Test
+        void should_map_correctly() {
+
+            // Given
+            List<Pair<LocalDate, LocalDate>> dateRangeInput = List.of(
+                    Pair.of(
+                            _2023_01_01,
+                            _2023_01_25
+                    ),
+                    Pair.of(
+                            _2010_01_01,
+                            _2015_01_01
+                    )
+            );
+
+            BeschaeftigungszeitListeType beschaeftigungszeitListe = getBeschaeftigungszeitListeMock(dateRangeInput);
+
+            final MitarbeiterType employee = createEmployee(beschaeftigungszeitListe);
+
+            // When
+            List<DateRange> mapped = mapper.getEmploymentPeriods(employee);
+
+            // Then
+            List<DateRange> expectedPeriods = List.of(
+                    DateRange.of(_2023_01_01, _2023_01_25),
+                    DateRange.of(_2010_01_01, _2015_01_01)
+            );
+
+            assertThat(mapped.size()).isEqualTo(expectedPeriods.size());
+            assertThat(mapped).containsAll(expectedPeriods);
+        }
+        @Test
+        void should_ignoreRangeWithoutEnd() {
+
+            // Given
+            List<Pair<LocalDate, LocalDate>> dateRangeInput = List.of(
+                    Pair.of(
+                            _2023_01_01,
+                            _2023_01_25
+                    ),
+                    Pair.of(
+                            _2010_01_01,
+                            null
+                    )
+            );
+
+
+            BeschaeftigungszeitListeType beschaeftigungszeitListe = getBeschaeftigungszeitListeMock(dateRangeInput);
+
+            final MitarbeiterType employee = createEmployee(beschaeftigungszeitListe);
+
+            // When
+            List<DateRange> mapped = mapper.getEmploymentPeriods(employee);
+
+            // Then
+            List<DateRange> expectedPeriods = List.of(
+                    DateRange.of(_2023_01_01, _2023_01_25)
+            );
+
+            assertThat(mapped.size()).isEqualTo(expectedPeriods.size());
+            assertThat(mapped).containsAll(expectedPeriods);
+        }
+
+        @Test
+        void should_mapToEmptyList_when_beschaeftigungszeitListeIsNull() {
+
+            // Given
+            final MitarbeiterType employee = createEmployee(null);
+
+            // When
+            List<DateRange> mapped = mapper.getEmploymentPeriods(employee);
+
+
+            assertThat(mapped).isNotNull();
+            assertThat(mapped).isEmpty();
+        }
+
+        private MitarbeiterType createEmployee(BeschaeftigungszeitListeType beschaeftigungszeitListe) {
+            final MitarbeiterType employee = new MitarbeiterType();
+            employee.setBeschaeftigungszeitListe(beschaeftigungszeitListe);
+            return employee;
+        }
+
+        private BeschaeftigungszeitListeType getBeschaeftigungszeitListeMock(List<Pair<LocalDate, LocalDate>> dateRangeInput) {
+            BeschaeftigungszeitListeType beschaeftigungszeitListeMock = Mockito.mock(BeschaeftigungszeitListeType.class);
+
+            List<BeschaeftigungszeitType> beschaeftigungszeit = dateRangeInput.stream().map(x -> {
+                BeschaeftigungszeitType bt = new BeschaeftigungszeitType();
+                bt.setStartdatum(Optional.ofNullable(x.getLeft()).map(LocalDate::toString).orElse(null));
+                bt.setEnddatum(Optional.ofNullable(x.getRight()).map(LocalDate::toString).orElse(null));
+
+                return bt;
+            }).collect(Collectors.toList());
+
+
+            // Mocking needed because no setter methods provided
+            Mockito.when(beschaeftigungszeitListeMock.getBeschaeftigungszeit()).thenReturn(beschaeftigungszeit);
+
+            return beschaeftigungszeitListeMock;
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for determing the exit date of given employment periods")
+    class ExitDateOfEmploymentPeriods {
+
+        @Test
+        void should_beNull_ifNoNull_isGiven() {
+
+            // Given
+            List <DateRange> periods = null;
+
+            // When
+            LocalDate exitDate = mapper.determineNewestExitDateOfEmploymentPeriods(periods);
+
+            // Then
+            assertThat(exitDate).isNull();
+        }
+
+        @Test
+        void should_beNull_ifEmptyList_isGiven() {
+
+            // Given
+            List <DateRange> periods = new ArrayList<>();
+
+            // When
+            LocalDate exitDate = mapper.determineNewestExitDateOfEmploymentPeriods(periods);
+
+            // Then
+            assertThat(exitDate).isNull();
+        }
+
+        @Test
+        void should_beNull_ifPeriodsInFuture() {
+
+            // Given
+            LocalDate now = LocalDate.now();
+            List <DateRange> periods = new ArrayList<>();
+
+            periods.add(DateRange.of(now.plusMonths(2), now.plusMonths(5)));
+            periods.add(DateRange.of(now.plusDays(2), now.plusMonths(5)));
+            periods.add(DateRange.of(now.plusYears(2), now.plusMonths(5)));
+
+
+            // When
+            LocalDate exitDate = mapper.determineNewestExitDateOfEmploymentPeriods(periods);
+
+            // Then
+            assertThat(exitDate).isNull();
+        }
+
+        @Test
+        void should_determineTheClosestExitDate_when_multipleEmploymentsInThePast_areGiven() {
+
+            // Given
+            LocalDate now = LocalDate.now();
+            LocalDate yesterday = now.minusDays(1);
+            List <DateRange> periods = new ArrayList<>();
+
+            periods.add(DateRange.of(now.minusYears(10), now.minusYears(8)));
+            periods.add(DateRange.of(now.minusYears(1), yesterday));
+            periods.add(DateRange.of(now.minusYears(5), now.minusYears(4)));
+
+            // When
+            LocalDate exitDate = mapper.determineNewestExitDateOfEmploymentPeriods(periods);
+
+            // Then
+            assertThat(exitDate).isEqualTo(yesterday);
+        }
+
+        @Test
+        void should_returnNull_ifEmployeeHasResignedButTerminationDateInFuture() {
+
+            // Given
+            LocalDate now = LocalDate.now();
+            LocalDate firstDayOfWork = now.minusYears(1);
+            LocalDate lastDayOfWork = now.plusMonths(2);
+            List <DateRange> periods = new ArrayList<>();
+
+            periods.add(DateRange.of(firstDayOfWork, lastDayOfWork));
+
+            // When
+            LocalDate exitDate = mapper.determineNewestExitDateOfEmploymentPeriods(periods);
+
+            // Then
+            assertThat(exitDate).isNull();
+        }
+
+        @Test
+        void should_mapExitDate_correctly() {
+
+            // Given
+            LocalDate now = LocalDate.now();
+            LocalDate yesterday = now.minusDays(1);
+
+            final MitarbeiterType employee = new MitarbeiterType();
+
+            BeschaeftigungszeitListeType employments = createBeschaeftigungszeitListeType(
+                    List.of(
+                            createBeschaeftigungszeitType(now.minusYears(10), now.minusYears(8)),
+                            createBeschaeftigungszeitType(now.minusYears(1), yesterday),
+                            createBeschaeftigungszeitType(now.minusYears(5), now.minusYears(4))
+
+                    ));
+            employee.setBeschaeftigungszeitListe(employments);
+
+            // When
+            Employee mapped = mapper.map(employee);
+
+            // Then
+            assertThat(mapped.getExitDate()).isEqualTo(yesterday);
+        }
     }
 
     private BeschaeftigungszeitType createBeschaeftigungszeitType(final LocalDate start, final LocalDate end) {
