7ed41b2930f4541544ca397d0d749b7b450cebb6
Simon Bauer <simon.bauer@gepardec.com>
2023-03-22 08:45:35 +0100

    [Gepardec/mega#644] EmployeeService & Impl Inaktive User (D.h. nicht mehr in ZEP) sollen bis hin zum monat, in dem sie gekündigt haben, in Projekt&Office Management angezeigt werden. Dafür gibt es jetzt die methode "getAllActiveEmployeesConsideringExitDate".

--- src/main/java/com/gepardec/mega/service/api/EmployeeService.java
+++ src/main/java/com/gepardec/mega/service/api/EmployeeService.java
@@ -2,6 +2,7 @@ package com.gepardec.mega.service.api;
 
 import com.gepardec.mega.domain.model.Employee;
 
+import java.time.YearMonth;
 import java.util.List;
 
 public interface EmployeeService {
@@ -9,6 +10,16 @@ public interface EmployeeService {
     Employee getEmployee(final String userId);
 
     List<Employee> getAllActiveEmployees();
+
+    /**
+     * Liefert auch inaktive Employees zurück, wenn deren exitdate >= selectedYearMonth ist
+     * Wird für die GUI benötigt.
+     * Wenn nur aktive Employees geliefert werden würden, dann könnte man die Zeiten von dem Employee
+     * nicht mehr kontrollieren, oder die Historie sehen, sobald der Mitarbeiter nicht mehr in ZEP existiert.
+     * @param selectedYearMonth ausgewählter Monat (von der GUI)
+     * @return Aktive Employees und inaktive Employees, die aber lt. den Kriterien von oben noch angezeigt werden sollen
+     */
+    List<Employee> getAllEmployeesConsideringExitDate(YearMonth selectedYearMonth);
 
     void updateEmployeeReleaseDate(final String userId, final String releaseDate);
 
