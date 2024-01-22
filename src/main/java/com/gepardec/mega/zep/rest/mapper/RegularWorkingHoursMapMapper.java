96c088fe4fae67d8764b058192afd5702c91ac4a
Tobias Meindl <to.meindl@gmx.at>
2024-01-22 15:50:55 +0100

    [Gepardec/mega#727] improve the RegularWorkingHours Mapper and add tests

--- src/main/java/com/gepardec/mega/zep/rest/mapper/RegularWorkingHoursMapMapper.java
+++ src/main/java/com/gepardec/mega/zep/rest/mapper/RegularWorkingHoursMapMapper.java
@@ -6,19 +6,21 @@ import java.time.DayOfWeek;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class RegularWorkingHoursMapMapper {
 
     public static Map<DayOfWeek, Duration> map(ZepRegularWorkingTimes zepRegularWorkingTimes) {
         Map<DayOfWeek, Duration> regularWorkingHours = new HashMap<>();
+
         if (zepRegularWorkingTimes != null) {
-            regularWorkingHours.put(DayOfWeek.MONDAY, Duration.ofHours(zepRegularWorkingTimes.getMonday().longValue()));
-            regularWorkingHours.put(DayOfWeek.TUESDAY, Duration.ofHours(zepRegularWorkingTimes.getTuesday().longValue()));
-            regularWorkingHours.put(DayOfWeek.WEDNESDAY, Duration.ofHours(zepRegularWorkingTimes.getWednesday().longValue()));
-            regularWorkingHours.put(DayOfWeek.THURSDAY, Duration.ofHours(zepRegularWorkingTimes.getThursday().longValue()));
-            regularWorkingHours.put(DayOfWeek.FRIDAY, Duration.ofHours(zepRegularWorkingTimes.getFriday().longValue()));
-            regularWorkingHours.put(DayOfWeek.SATURDAY, Duration.ofHours(zepRegularWorkingTimes.getSaturday().longValue()));
-            regularWorkingHours.put(DayOfWeek.SUNDAY, Duration.ofHours(zepRegularWorkingTimes.getSunday().longValue()));
+            regularWorkingHours.put(DayOfWeek.MONDAY, Duration.ofHours(zepRegularWorkingTimes.getMonday() == null ? 0 : zepRegularWorkingTimes.getMonday().longValue()));
+            regularWorkingHours.put(DayOfWeek.TUESDAY, Duration.ofHours(zepRegularWorkingTimes.getTuesday() == null ? 0 : zepRegularWorkingTimes.getTuesday().longValue()));
+            regularWorkingHours.put(DayOfWeek.WEDNESDAY, Duration.ofHours(zepRegularWorkingTimes.getWednesday() == null ? 0 : zepRegularWorkingTimes.getWednesday().longValue()));
+            regularWorkingHours.put(DayOfWeek.THURSDAY, Duration.ofHours(zepRegularWorkingTimes.getThursday() == null ? 0 : zepRegularWorkingTimes.getThursday().longValue()));
+            regularWorkingHours.put(DayOfWeek.FRIDAY, Duration.ofHours(zepRegularWorkingTimes.getFriday() == null ? 0 : zepRegularWorkingTimes.getFriday().longValue()));
+            regularWorkingHours.put(DayOfWeek.SATURDAY, Duration.ofHours(zepRegularWorkingTimes.getSaturday() == null ? 0 : zepRegularWorkingTimes.getSaturday().longValue()));
+            regularWorkingHours.put(DayOfWeek.SUNDAY, Duration.ofHours(zepRegularWorkingTimes.getSunday() == null ? 0 : zepRegularWorkingTimes.getSunday().longValue()));
         }
         return regularWorkingHours;
     }
---
+++ src/test/java/com/gepardec/mega/zep/rest/mapper/RegularWorkingHoursMapMapperTests.java
@@ -0,0 +1,93 @@
+package com.gepardec.mega.zep.rest.mapper;
+
+import com.gepardec.mega.zep.rest.entity.ZepRegularWorkingTimes;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Map;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@QuarkusTest
+public class RegularWorkingHoursMapMapperTests {
+
+
+
+    @Test
+    public void mapZepRegularWorkingTimesToRegularWorkingHoursMap() {
+        ZepRegularWorkingTimes zepRegularWorkingTimes = ZepRegularWorkingTimes.builder()
+                .id(1)
+                .employee_id("001")
+                .start_date(LocalDateTime.of(2019, 1, 2, 8, 1, 32))
+                .monday(8.0)
+                .tuesday(8.0)
+                .wednesday(8.0)
+                .thursday(8.0)
+                .friday(8.0)
+                .saturday(0.0)
+                .sunday(0.0)
+                .is_monthly(false)
+                .monthly_hours(0.0)
+                .max_hours_in_month(0.0)
+                .max_hours_in_week(40.0)
+                .build();
+
+        Map<DayOfWeek, Duration> regularWorkingHours = RegularWorkingHoursMapMapper.map(zepRegularWorkingTimes);
+
+        assertThat(regularWorkingHours.get(DayOfWeek.MONDAY)).isEqualTo(Duration.ofHours(8));
+        assertThat(regularWorkingHours.get(DayOfWeek.TUESDAY)).isEqualTo(Duration.ofHours(8));
+        assertThat(regularWorkingHours.get(DayOfWeek.WEDNESDAY)).isEqualTo(Duration.ofHours(8));
+        assertThat(regularWorkingHours.get(DayOfWeek.THURSDAY)).isEqualTo(Duration.ofHours(8));
+        assertThat(regularWorkingHours.get(DayOfWeek.FRIDAY)).isEqualTo(Duration.ofHours(8));
+        assertThat(regularWorkingHours.get(DayOfWeek.SATURDAY)).isEqualTo(Duration.ofHours(0));
+        assertThat(regularWorkingHours.get(DayOfWeek.SUNDAY)).isEqualTo(Duration.ofHours(0));
+
+    }
+
+    @Test
+    public void mapZepRegularWorkingHoursToRegularWorkingHoursMap_NullMappedToZero() {
+        ZepRegularWorkingTimes zepRegularWorkingTimes = ZepRegularWorkingTimes.builder()
+                .id(1)
+                .employee_id("001")
+                .start_date(LocalDateTime.of(2019, 1, 2, 8, 1, 32))
+                .monday(null)
+                .tuesday(8.0)
+                .wednesday(8.0)
+                .thursday(8.0)
+                .friday(8.0)
+                .saturday(null)
+                //Sunday missing -> null
+                .is_monthly(false)
+                .monthly_hours(0.0)
+                .max_hours_in_month(0.0)
+                .max_hours_in_week(40.0)
+                .build();
+
+        Map<DayOfWeek, Duration> regularWorkingHours = RegularWorkingHoursMapMapper.map(zepRegularWorkingTimes);
+
+        assertThat(regularWorkingHours.get(DayOfWeek.MONDAY)).isEqualTo(Duration.ofHours(0));
+        assertThat(regularWorkingHours.get(DayOfWeek.TUESDAY)).isEqualTo(Duration.ofHours(8));
+        assertThat(regularWorkingHours.get(DayOfWeek.WEDNESDAY)).isEqualTo(Duration.ofHours(8));
+        assertThat(regularWorkingHours.get(DayOfWeek.THURSDAY)).isEqualTo(Duration.ofHours(8));
+        assertThat(regularWorkingHours.get(DayOfWeek.FRIDAY)).isEqualTo(Duration.ofHours(8));
+        assertThat(regularWorkingHours.get(DayOfWeek.SATURDAY)).isEqualTo(Duration.ofHours(0));
+        assertThat(regularWorkingHours.get(DayOfWeek.SUNDAY)).isEqualTo(Duration.ofHours(0));
+    }
+
+    @Test
+    public void mapZepRegularWorkingHoursToRegularWorkingHoursMap_EmptyObjectMappedToZero() {
+        ZepRegularWorkingTimes zepRegularWorkingTimes = ZepRegularWorkingTimes.builder().build();
+
+        Map<DayOfWeek, Duration> regularWorkingHours = RegularWorkingHoursMapMapper.map(zepRegularWorkingTimes);
+
+        assertThat(regularWorkingHours.get(DayOfWeek.MONDAY)).isEqualTo(Duration.ofHours(0));
+        assertThat(regularWorkingHours.get(DayOfWeek.TUESDAY)).isEqualTo(Duration.ofHours(0));
+        assertThat(regularWorkingHours.get(DayOfWeek.WEDNESDAY)).isEqualTo(Duration.ofHours(0));
+        assertThat(regularWorkingHours.get(DayOfWeek.THURSDAY)).isEqualTo(Duration.ofHours(0));
+        assertThat(regularWorkingHours.get(DayOfWeek.FRIDAY)).isEqualTo(Duration.ofHours(0));
+        assertThat(regularWorkingHours.get(DayOfWeek.SATURDAY)).isEqualTo(Duration.ofHours(0));
+        assertThat(regularWorkingHours.get(DayOfWeek.SUNDAY)).isEqualTo(Duration.ofHours(0));
+    }
+}
