b14a8eaff7a3329dcd8806050562a6b571b1ae8a
sgartner03 <simon.gartner@gepardec.com>
2024-01-25 14:20:16 +0100

    [gepardec/mega#727] Added EmployeeType

---
+++ src/main/java/com/gepardec/mega/zep/rest/entity/ZepProjectEmployeeType.java
@@ -0,0 +1,12 @@
+package com.gepardec.mega.zep.rest.entity;
+
+import lombok.*;
+
+@Getter @Setter
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+public class ZepProjectEmployeeType {
+    private int id;
+
+    private String name;
+}
