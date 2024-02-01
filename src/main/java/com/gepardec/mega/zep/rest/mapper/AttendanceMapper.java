c022270f29d9aeca036aec5159b82c6465843305
Tobias Meindl <to.meindl@gmx.at>
2024-02-01 14:35:19 +0100

    [Gepardec/mega#727] make mapper application scoped

--- src/main/java/com/gepardec/mega/zep/rest/mapper/AttendanceMapper.java
+++ src/main/java/com/gepardec/mega/zep/rest/mapper/AttendanceMapper.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@ApplicationScoped
 public class AttendanceMapper implements Mapper<ProjectTime, ZepAttendance> {
 
     @Override
