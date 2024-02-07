885edfdb3c12ffdeacc7dfb85bc6130fb2a5816a
sgartner03 <simon.gartner@gepardec.com>
2024-02-07 17:51:40 +0100

    [gepardec/mega#727] Added ResourceFileService

---
+++ src/main/java/com/gepardec/mega/zep/util/files/ResourceFileService.java
@@ -0,0 +1,58 @@
+package com.gepardec.mega.zep.util.files;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Dependent;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Dependent
+public class ResourceFileService {
+    private File filesDir;
+
+    public File getFilesDir() {
+        return filesDir;
+    }
+
+    public ResourceFileService(@ResourcePath String filesPath) {
+        File filesDir = getFileOfResourcesPath(filesPath);
+        if (filesDir.isDirectory()) {
+            this.filesDir = filesDir;
+        }
+    }
+
+    public Optional<String> getSingleFile(String path) {
+        File file = new File(filesDir.getPath() + "/" + path);
+        System.out.println(file.getPath());
+        if (!file.isFile()) {
+            return Optional.empty();
+        }
+        return Optional.of(readFile(file));
+    }
+
+    public List<String> getDirContents(String path) {
+        File filesSubDir = new File(filesDir.getPath() + "/" + path);
+        return Arrays.stream(filesSubDir.listFiles())
+                .map(ResourceFileService::readFile)
+                .toList();
+    }
+
+
+    private static String readFile(File f) {
+        try {
+            return FileUtils.readFileToString(f, "UTF-8");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private File getFileOfResourcesPath(String path) {
+        String resourcesPath = this.getClass().getResource("/").getPath();
+        String absPath = resourcesPath + path;
+        return new File(absPath);
+    }
+}
---
+++ src/test/java/com/gepardec/mega/zep/rest/util/ResourceFileServiceTests.java
@@ -0,0 +1,39 @@
+package com.gepardec.mega.zep.rest.util;
+
+import com.gepardec.mega.zep.util.files.ResourceFileService;
+import com.gepardec.mega.zep.util.files.ResourcePath;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Produces;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@QuarkusTest
+public class ResourceFileServiceTests {
+    @Inject
+    ResourceFileService resourceFileService;
+
+    @Test
+    public void testEnv_thenReturnCorrectJsonRespPath() {
+        String expectedPath = this.getClass().getResource("/zep/rest/testresponses").getPath();
+        assertThat(resourceFileService.getFilesDir().getPath()).isEqualTo(expectedPath);
+    }
+
+    @Test
+    public void testEnv_thenReturnCorrectFileContent() {
+        assertThat(resourceFileService.getSingleFile("_test").get()).startsWith("ok");
+    }
+
+    @Test
+    public void testEnv_thenReturnCorrectDirContents() {
+        resourceFileService.getDirContents("_dirtest").forEach(
+            content -> assertThat(content).startsWith("ok")
+        );
+    }
+
+
+}
