3791e4879261a4e00d17294c1337aecd0d4e9c6b
Tobias Meindl <tobias.meindl@gepardec.com>
2024-03-27 15:28:43 +0100

    Changes by simongartner

--- src/main/java/com/gepardec/mega/zep/util/JsonUtil.java
+++ src/main/java/com/gepardec/mega/zep/util/JsonUtil.java
@@ -3,6 +3,7 @@ package com.gepardec.mega.zep.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.gepardec.mega.zep.ZepServiceException;
 import java.util.Optional;
 
@@ -12,7 +13,6 @@ public class JsonUtil {
     public static <T> Optional<T> parseJson (String json, String path, Class<T> resultClass) {
         var objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules();
-
         try {
             JsonNode jsonNode = objectMapper.readTree(json).at(path);
             if (jsonNode.isMissingNode()) {
@@ -28,10 +28,11 @@ public class JsonUtil {
             }
 
             return Optional.of(objectMapper.treeToValue(jsonNode, resultClass));
-        } catch (JsonProcessingException e) {
+        }  catch (JsonProcessingException e) {
+            if (e instanceof MismatchedInputException mme) {
+                throw new RuntimeException(mme.getMessage());
+            }
             throw new ZepServiceException("Error while parsing json to " + resultClass.getName(), e);
         }
     }
-
-
 }
