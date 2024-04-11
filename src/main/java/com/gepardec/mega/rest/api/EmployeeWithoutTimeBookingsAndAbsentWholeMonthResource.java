be3e36adcae3fadc8551257509697c3c5b5d8ead
gattrCh <gattringerchiara0@gmail.com>
2024-04-11 13:30:09 +0200

    [Gepardec/mega#699] add if and impl class for rest

---
+++ src/main/java/com/gepardec/mega/rest/api/EmployeeWithoutTimeBookingsAndAbsentWholeMonthResource.java
@@ -0,0 +1,4 @@
+package com.gepardec.mega.rest.api;
+
+public interface EmployeeWithoutTimeBookingsAndAbsentWholeMonthResource {
+}
---
+++ src/main/java/com/gepardec/mega/rest/impl/EmployeeWithoutTimeBookingsAndAbsentWholeMonthResourceImpl.java
@@ -0,0 +1,6 @@
+package com.gepardec.mega.rest.impl;
+
+import com.gepardec.mega.rest.api.EmployeeWithoutTimeBookingsAndAbsentWholeMonthResource;
+
+public class EmployeeWithoutTimeBookingsAndAbsentWholeMonthResourceImpl implements EmployeeWithoutTimeBookingsAndAbsentWholeMonthResource {
+}
