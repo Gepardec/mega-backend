a944db0e8f03f8470266dc04099abbe4a99c50bd
Christoph Ruhsam <christoph.ruhsam@gepardec.com>
2024-04-22 09:38:00 +0200

    #674 remove import

--- src/test/java/com/gepardec/mega/zep/ZepServiceImplTest.java
+++ src/test/java/com/gepardec/mega/zep/ZepServiceImplTest.java
@@ -50,7 +50,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
