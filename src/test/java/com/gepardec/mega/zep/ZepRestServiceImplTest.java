bf2915b89915a5156a41260c7e9fc70a69456768
gattrCh <gattringerchiara0@gmail.com>
2024-05-23 13:45:44 +0200

    [Gepardec/mega#744] add service tests and remove unused imports

--- src/test/java/com/gepardec/mega/zep/ZepRestServiceImplTest.java
+++ src/test/java/com/gepardec/mega/zep/ZepRestServiceImplTest.java
@@ -1,17 +1,28 @@
 package com.gepardec.mega.zep;
 
+import com.gepardec.mega.db.entity.common.ProjectTaskType;
+import com.gepardec.mega.domain.model.Employee;
 import com.gepardec.mega.domain.model.ProjectTime;
 import com.gepardec.mega.zep.impl.Rest;
-import com.gepardec.mega.zep.rest.client.ZepEmployeeRestClient;
+import com.gepardec.mega.zep.rest.dto.ZepAttendance;
+import com.gepardec.mega.zep.rest.service.AttendanceService;
 import com.gepardec.mega.zep.rest.service.RegularWorkingTimesService;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
 import jakarta.inject.Inject;
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 
 @QuarkusTest
 public class ZepRestServiceImplTest {
@@ -20,6 +31,9 @@ public class ZepRestServiceImplTest {
 
     @Inject @Rest
     ZepService zepRestService;
+
+    @InjectMock
+    AttendanceService attendanceService;
 
 //    @Inject
 //    ZepEmployeeRestClient zepEmployeeRestClient;
@@ -39,5 +53,48 @@ public class ZepRestServiceImplTest {
             System.out.println(projectTime.getUserId() + ": " + projectTime.getDuration() + " " + projectTime.getBillable());
         }
         System.out.println(projectTimes.size());
+    }
+
+    @Test
+    public void getDoctorsVisitingTimeForMonthAndEmployee_whenUserHadDoctorsAppointments_thenReturnHours() {
+        when(attendanceService.getAttendanceForUserProjectAndMonth(anyString(), any(LocalDate.class), anyInt()))
+                .thenReturn(getZepAttendancesForDoctorsAppointment());
+
+        double actual = zepRestService.getDoctorsVisitingTimeForMonthAndEmployee(createEmployee(), YearMonth.of(2024, 5));
+
+        assertThat(actual).isEqualTo(3.75);
+    }
+
+    @Test
+    public void getDoctorsVisitingTimeForMonthAndEmployee_whenUserHadNoDoctorsAppointments_thenReturnZeroHours() {
+        when(attendanceService.getAttendanceForUserProjectAndMonth(anyString(), any(LocalDate.class), anyInt()))
+                .thenReturn(List.of());
+
+        double actual = zepRestService.getDoctorsVisitingTimeForMonthAndEmployee(createEmployee(), YearMonth.of(2024, 5));
+
+        assertThat(actual).isEqualTo(0.0);
+    }
+
+    private ZepAttendance createZepAttendance(double duration) {
+        return ZepAttendance.builder()
+                            .id(1)
+                            .duration(duration)
+                            .projectId(ProjectTaskType.PROJECT_INTERNAL.getId())
+                            .projectTaskId(ProjectTaskType.TASK_DOCTOR_VISIT.getId())
+                            .build();
+    }
+    private List<ZepAttendance> getZepAttendancesForDoctorsAppointment() {
+        return List.of(
+            createZepAttendance(1.0),
+            createZepAttendance(2.0),
+            createZepAttendance(0.5),
+            createZepAttendance(0.25)
+        );
+    }
+
+    private Employee createEmployee() {
+        return Employee.builder()
+                       .userId("testUser2")
+                       .build();
     }
 }
