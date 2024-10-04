3ca823bd6976f38efe034dc868895f8178daac87
Tod Oliver <olivertod11@yahoo.de>
2024-10-04 11:47:43 +0200

    [Gepardec/mega#755] remove unused classes, use concrete mapper classes

--- src/test/java/com/gepardec/mega/zep/rest/mapper/ProjectTimeMapperTest.java
+++ src/test/java/com/gepardec/mega/zep/rest/mapper/ProjectTimeMapperTest.java
@@ -10,20 +10,21 @@ import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AttendanceMapperTest {
+class ProjectTimeMapperTest {
 
     @Test
     void withFullSettings_thenReturnsAbsenceTimeObject() {
-        AttendanceMapper attendanceMapper = new AttendanceMapper();
-        ZepAttendanceDirectionOfTravel zepAttendanceDirectionOfTravel = ZepAttendanceDirectionOfTravel.builder()
-                                                                                                    .id("2")
-                                                                                                    .name("return")
-                                                                                                    .build();
+        ProjectTimeMapper projectTimeMapper = new ProjectTimeMapper();
+        ZepAttendanceDirectionOfTravel zepAttendanceDirectionOfTravel =
+                ZepAttendanceDirectionOfTravel.builder()
+                        .id("2")
+                        .name("return")
+                        .build();
         ZepAttendance zepAttendance = ZepAttendance.builder()
                 .id(2)
-                .date(LocalDate.of(2015,2,11))
-                .from(LocalTime.of(9,0,0))
-                .to(LocalTime.of(13,45,0))
+                .date(LocalDate.of(2015, 2, 11))
+                .from(LocalTime.of(9, 0, 0))
+                .to(LocalTime.of(13, 45, 0))
                 .employeeId("001-duser")
                 .projectId(1)
                 .projectTaskId(2)
@@ -36,7 +37,7 @@ class AttendanceMapperTest {
                 .directionOfTravel(zepAttendanceDirectionOfTravel)
                 .build();
 
-        ProjectTime pt = attendanceMapper.map(zepAttendance);
+        ProjectTime pt = projectTimeMapper.map(zepAttendance);
 
         assertThat(pt.getUserId()).isEqualTo(zepAttendance.employeeId());
         assertThat(pt.getDuration()).isEqualTo("04:45");
