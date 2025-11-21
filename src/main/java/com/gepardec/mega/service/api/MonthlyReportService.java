6ab155bd2fbd6e8aa86ad06440016b93f15b1324
Oliver Tod <olivertod11@yahoo.de>
2025-11-21 16:30:59 +0100

    [Gepardec/mega#739] implement attendances

--- src/main/java/com/gepardec/mega/service/api/MonthlyReportService.java
+++ src/main/java/com/gepardec/mega/service/api/MonthlyReportService.java
@@ -1,5 +1,6 @@
 package com.gepardec.mega.service.api;
 
+import com.gepardec.mega.domain.model.Attendances;
 import com.gepardec.mega.domain.model.EmployeeCheck;
 import com.gepardec.mega.domain.model.monthlyreport.MonthlyReport;
 
@@ -10,4 +11,6 @@ public interface MonthlyReportService {
     MonthlyReport getMonthEndReportForUser(YearMonth payrollMonth);
 
     EmployeeCheck getEmployeeCheck(YearMonth payrollMonth);
+
+    Attendances getAttendances(YearMonth payrollMonth);
 }
